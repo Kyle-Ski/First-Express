40f72f8bd3d698703d869cb340a34b9159d3b37a
Kyle Czajkowski <skiroyjenkins@gmail.com>
2018-10-22 16:12:59 -0600

    added better error handling and a better way to use req.params

--- index.js
+++ index.js
@@ -18,11 +18,25 @@ app.get('/', (req, res, next) => {
 })
 
 app.get('/:index', (req, res, next) => {
-    if (req.params.index <= data.length){
-        res.send(data[req.params.index - 1])
-    } else {
-        res.status(404)
-        res.redirect("https://http.cat/404")    
+    const id = req.params.index
+    if(!(Number(id))){
+        next()
     }
+    const student = data.filter(student => student.id == id)[0]
+    res.json({data: student})
 })
+
+app.use(notFound)
+app.use(errorHandler)
+
+function notFound(req, res, next) {
+    res.status(404).send({error: 'Not found!', status: 404, url: req.originalUrl})
+}
+
+function errorHandler(err, req, res, next) {
+    console.error('ERROR', err)
+    const stack =  process.env.NODE_ENV !== 'production' ? err.stack : undefined
+    res.status(500).send({error: err.message, stack, url: req.originalUrl})
+}
+
 app.listen(port, () => console.log(`I got you on http://localhost:${port}`))
